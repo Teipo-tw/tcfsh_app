93ec3d510a89e604e228b3daa23438c8a15a187a
ChenSC0392 <chensc0392@gmail.com>
2021-03-11 11:01:11 +0800

    home page first

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'TCFSH APP'),
     );
   }
 }
@@ -50,11 +50,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -77,37 +72,241 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+           mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:[
+            Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children:[
+                    Container(
+                        width: 320,
+                        height: 60,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25), bottomLeft: Radius.circular(0), bottomRight: Radius.circular(0), ),
+                            color: Color(0xff84d5fa),
+                        ),
+                        child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children:[
+                                Text(
+                                    "109年12月4日",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 34,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w700,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                    SizedBox(height: 5),
+                    Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(0), topRight: Radius.circular(0), bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25), ),
+                            color: Color(0xffd7dfe2),
+                        ),
+                        padding: const EdgeInsets.only(left: 30, right: 10, top: 10, bottom: 20, ),
+                        child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:[
+                                Text(
+                                    "本日事項",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 24,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w700,
+                                    ),
+                                ),
+                                SizedBox(height: 5),
+                                SizedBox(
+                                    width: 280,
+                                    child: Text(
+                                        " ‧ 廁所綠、美化競賽。\n ‧ 校內國語文競賽。\n ‧ 環境教育。",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontFamily: "Noto Sans CJK TC",
+                                            fontWeight: FontWeight.w500,
+                                        ),
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 20),
+            Container(
+                width: 300,
+                height: 75,
+                child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children:[
+                        Container(
+                            width: 300,
+                            height: 75,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Color(0xffd8dfe3),
+                            ),
+                            padding: const EdgeInsets.symmetric(horizontal: 85, vertical: 20, ),
+                            child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children:[
+                                    Text(
+                                        "􀅴 學生資訊",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 24,
+                                        ),
+                                    ),
+                                ],
+                            ),
+                        ),
+                    ],
+                ),
             ),
-          ],
+            SizedBox(height: 20),
+            Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children:[
+                    Container(
+                        width: 145,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffd8dfe3),
+                        ),
+                        padding: const EdgeInsets.only(left: 15, top: 10, ),
+                        child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:[
+                                Text(
+                                    "校園公告",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w500,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                        width: 145,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffd8dfe3),
+                        ),
+                        padding: const EdgeInsets.only(left: 15, top: 10, ),
+                        child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:[
+                                Text(
+                                    "數位校園",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w500,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                ],
+            ),
+            SizedBox(height: 20),
+            Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children:[
+                    Container(
+                        width: 145,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffd8dfe3),
+                        ),
+                        padding: const EdgeInsets.only(left: 15, top: 10, ),
+                        child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:[
+                                Text(
+                                    "電視牆公告",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w500,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                        width: 145,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffd8dfe3),
+                        ),
+                        padding: const EdgeInsets.only(left: 15, top: 10, ),
+                        child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:[
+                                Text(
+                                    "閱覽室\n選位系統",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w500,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                ],
+            ),
+        ],
+
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
