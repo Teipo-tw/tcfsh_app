dbe53850342844718b0ea764cd468cbae94e6a35
tw0894big <handsometom.yee@gmail.com>
2021-03-18 20:15:09 +0800

    123

--- lib/page_announcement_main.dart
+++ lib/page_announcement_main.dart
@@ -2,154 +2,151 @@
 
 import 'package:flutter/material.dart';
 
-
-class PageAnnouncementMain extends StatelessWidget{
+class PageAnnouncementMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: new InkWell(
-          child: Row(
-            children: <Widget>[
-              Icon(Icons.keyboard_arrow_left),
-              Text('返回'),
-            ]
-          ),
-          onTap: (){
+        appBar: AppBar(
+            leading: new InkWell(
+          child: Row(children: <Widget>[
+            Icon(Icons.keyboard_arrow_left),
+            Text('返回'),
+          ]),
+          onTap: () {
             Navigator.pop(context);
           },
-        )
-      ),
-      body: ListView.separated(
-          itemBuilder: (BuildContext context, int index){
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Color(0xffbae6ff),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10, ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:[
-                  Container(
-                    color: Color(0x00ffffff),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children:[
-                        Text(
+        )),
+        body: ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color(0xffbae6ff),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      color: Color(0x00ffffff),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
                             "2021-02-09",
                             style: TextStyle(
-                                color: Color(0xbf000000),
-                                fontSize: 15,
-                                fontFamily: "Noto Sans CJK TC",
-                                fontWeight: FontWeight.w500,
+                              color: Color(0xbf000000),
+                              fontSize: 15,
+                              fontFamily: "Noto Sans CJK TC",
+                              fontWeight: FontWeight.w500,
                             ),
-                        ),
-                        SizedBox(width: 5),
-                        Container(
+                          ),
+                          SizedBox(width: 5),
+                          Container(
                             width: 38,
                             height: 17,
                             color: Color(0x00ffffff),
                             child: Stack(
-                                children:[
-                                    Container(
-                                        width: 38,
-                                        height: 17,
-                                        decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(3),
-                                            color: Color(0xffcb333b),
-                                        ),
+                              children: [
+                                Container(
+                                  width: 38,
+                                  height: 17,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(3),
+                                    color: Color(0xffcb333b),
+                                  ),
+                                ),
+                                Positioned.fill(
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      "重要",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 12,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w500,
+                                      ),
                                     ),
-                                    Positioned.fill(
-                                        child: Align(
-                                            alignment: Alignment.topLeft,
-                                            child: Text(
-                                                "重要",
-                                                style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 12,
-                                                    fontFamily: "Noto Sans CJK TC",
-                                                    fontWeight: FontWeight.w500,
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                ],
+                                  ),
+                                ),
+                              ],
                             ),
-                        ),
-                        SizedBox(width: 5),
-                        Container(
+                          ),
+                          SizedBox(width: 5),
+                          Container(
                             width: 38,
                             height: 17,
                             color: Color(0x00ffffff),
                             child: Stack(
-                                children:[
-                                    Container(
-                                        width: 38,
-                                        height: 17,
-                                        decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(3),
-                                            color: Color(0xffe39900),
-                                        ),
+                              children: [
+                                Container(
+                                  width: 38,
+                                  height: 17,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(3),
+                                    color: Color(0xffe39900),
+                                  ),
+                                ),
+                                Positioned.fill(
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      "熱門",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 12,
+                                        fontFamily: "Noto Sans CJK TC",
+                                        fontWeight: FontWeight.w500,
+                                      ),
                                     ),
-                                    Positioned.fill(
-                                        child: Align(
-                                            alignment: Alignment.topLeft,
-                                            child: Text(
-                                                "熱門",
-                                                style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 12,
-                                                    fontFamily: "Noto Sans CJK TC",
-                                                    fontWeight: FontWeight.w500,
-                                                ),
-                                            ),
-                                        ),
-                                    ),
-                                ],
+                                  ),
+                                ),
+                              ],
                             ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Container(
-                    width: 305,
-                    height: 1,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  SizedBox(
-                    width: 305,
-                    child: Text(
-                      "圖書館招募愛心媽媽義工～歡迎報名～$index",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontFamily: "Noto Sans CJK TC",
-                        fontWeight: FontWeight.w500,
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index){
-            return Align(
-            alignment : Alignment.centerLeft,
-              child: FlutterLogo(),
-            );
-          },
-          itemCount: 20
-        )
-    );
+                    SizedBox(height: 5),
+                    Container(
+                      width: 305,
+                      height: 1,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    SizedBox(
+                      width: 305,
+                      child: Text(
+                        "圖書館招募愛心媽媽義工～歡迎報名～$index",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontFamily: "Noto Sans CJK TC",
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return Align(
+                  alignment: Alignment.centerLeft,
+                  child: SizedBox(height: 1) //FlutterLogo(),
+                  );
+            },
+            itemCount: 20));
   }
 }
