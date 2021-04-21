ab34157519ff17a5179890da6cb1fc2c6bc142f3
ChenSC0392 <chensc0392@gmail.com>
2021-04-21 18:49:50 +0800

    tv page http and pubspec add http package

--- lib/page_tv_wall.dart
+++ lib/page_tv_wall.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class PageTVWall extends StatelessWidget {
+
+class PageTVWall extends StatefulWidget {
+  PageTVWall({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _PageTVWallState createState() => _PageTVWallState();
+}
+
+class _PageTVWallState extends State<PageTVWall> {
+  String data="321";
+
+  void getData() async {
+    var url = Uri.parse('https://tcirc.tw/');
+    setState(() {data = "12111";});
+    var response = await http.get(url);
+
+    var res = response.body;
+    //print('Response status: ${response.statusCode}');
+    //print('Response body: ${response.body}');
+    setState(() {data = res;});
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("聊天室頁"),
-    );
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("123"),
+        ),
+        body: new Center(
+          child: new Column(
+            children: [
+              Text("$data"),
+              IconButton(icon: Icon(Icons.home), onPressed: getData),
+            ],
+          ),
+        ));
   }
 }
