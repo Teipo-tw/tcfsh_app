cf1906311ea4762a0766d87058a94dfcb4705266
Handsometom <handsometom.yee@gmail.com>
2021-05-01 14:43:54 +0800

    去除奧步、修好界面

--- lib/page_announcement_main.dart
+++ lib/page_announcement_main.dart
@@ -15,7 +15,7 @@ class _PageAnnouncementMainState extends State<PageAnnouncementMain> {
 
   @override
   void initState() {
-    //監聽滾動事件，列印滾動位置
+    //監聽滾動事件
     _scrollController.addListener(() {
       print(_scrollController.offset); //列印滾動位置
     });
@@ -23,7 +23,7 @@ class _PageAnnouncementMainState extends State<PageAnnouncementMain> {
 
   @override
   void dispose() {
-    //為了避免記憶體洩露，需要呼叫_controller.dispose
+    //避免記憶體洩露，呼叫_scrollController.dispose
     _scrollController.dispose();
     super.dispose();
   }
@@ -57,12 +57,13 @@ class _PageAnnouncementMainState extends State<PageAnnouncementMain> {
                       children: <Widget>[
                         Icon(
                           Icons.arrow_back_ios,
+                          color: Colors.blue,
                           size: 24,
                         ),
                         Text(
                           "返回",
                           style: TextStyle(
-                            color: Color(0xbf000000),
+                            color: Colors.blue,
                             fontSize: 18,
                             fontFamily: "Noto Sans CJK TC",
                             fontWeight: FontWeight.w500,
@@ -80,7 +81,7 @@ class _PageAnnouncementMainState extends State<PageAnnouncementMain> {
                     Text(
                       "校園公告",
                       style: TextStyle(
-                        color: Color(0xbf000000),
+                        color: Colors.black,
                         fontSize: 34,
                         fontFamily: "Noto Sans CJK TC",
                         fontWeight: FontWeight.w700,
@@ -95,78 +96,110 @@ class _PageAnnouncementMainState extends State<PageAnnouncementMain> {
       ),
       body: ListView.separated(
         controller: _scrollController,
+        padding: EdgeInsets.only(top: 15),
         itemBuilder: (BuildContext context, int index) {
-          return MaterialButton(
-            //公告按鈕
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return PageTest();
-              }));
-            },
-            color: Color(0xffbae6ff),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 10,
-            ),
-            child: Column(
-              //全部物件垂直排列
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  //日期、狀態
+          return Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              MaterialButton(
+                //公告按鈕
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return PageTest();
+                  }));
+                },
+                color: Color(0xffbae6ff),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: Column(
+                  //全部物件垂直排列
                   mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "2021-02-09",
-                      style: TextStyle(
-                        color: Color(0xbf000000),
-                        fontSize: 15,
-                        fontFamily: "Noto Sans CJK TC",
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    SizedBox(width: 5),
-                    Container(
-                      //狀態：重要
-                      width: 38,
-                      height: 18,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(3),
-                        color: Color(0xffcb333b),
-                      ),
-                      alignment: Alignment.center,
-                      child: Text(
-                        "重要",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12,
-                          fontFamily: "Noto Sans CJK TC",
-                          fontWeight: FontWeight.w500,
+                    Row(
+                      //日期、狀態
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "2021-02-09",
+                          style: TextStyle(
+                            color: Color(0xbf000000),
+                            fontSize: 15,
+                            fontFamily: "Noto Sans CJK TC",
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
+                        SizedBox(width: 5),
+                        Container(
+                          //狀態：重要
+                          width: 38,
+                          height: 18,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(3),
+                            color: Color(0xffcb333b),
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "重要",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontFamily: "Noto Sans CJK TC",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Container(
+                          //狀態：熱門
+                          width: 38,
+                          height: 18,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(3),
+                            color: Color(0xffe39900),
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "熱門",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontFamily: "Noto Sans CJK TC",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Container(
+                      //分隔線
+                      width: 305,
+                      height: 1,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
                       ),
                     ),
-                    SizedBox(width: 5),
-                    Container(
-                      //狀態：熱門
-                      width: 38,
-                      height: 18,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(3),
-                        color: Color(0xffe39900),
-                      ),
-                      alignment: Alignment.center,
+                    SizedBox(height: 5),
+                    SizedBox(
+                      //公告標題
+                      width: 305,
                       child: Text(
-                        "熱門",
+                        "圖書館招募愛心媽媽義工～歡迎報名～$index",
                         style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12,
+                          color: Colors.black,
+                          fontSize: 15,
                           fontFamily: "Noto Sans CJK TC",
                           fontWeight: FontWeight.w500,
                         ),
@@ -174,32 +207,8 @@ class _PageAnnouncementMainState extends State<PageAnnouncementMain> {
                     ),
                   ],
                 ),
-                SizedBox(height: 5),
-                Container(
-                  //分隔線
-                  width: 305,
-                  height: 1,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 5),
-                SizedBox(
-                  //公告標題
-                  width: 305,
-                  child: Text(
-                    "圖書館招募愛心媽媽義工～歡迎報名～$index",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 15,
-                      fontFamily: "Noto Sans CJK TC",
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           );
         },
         itemCount: 50,
